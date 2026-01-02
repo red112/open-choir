a3a42515a9e06c783c5b4aebaad0bb2f6f6ae78c
red112 <red112@naver.com>
2026-01-02 12:19:39 +0900

    -

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import './i18n'; // import 추가
+import './i18n'; // 다국어 설정
+import { HelmetProvider } from 'react-helmet-async'; // [필수] 이 줄이 있어야 합니다!
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* [필수] App을 HelmetProvider로 감싸야 합니다! */}
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
 );
